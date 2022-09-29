691c7d90d6677c93a64551451def2f5065476e34
Frank Liu <lfl@lfs-MacBook-Air.local>
2022-09-29 12:08:21 +0800

    feat: java variables

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,24 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        boolean isMikeIsFemale = true;
+        byte num1 = -100;
+        short num2 = 30000;
+        int num3 = 1000000000;
+        long num4 = 1000000000;
+        float num5 = 100.10000f;
+        double num6 = 10.30000111111111111111111;
+        char char1 = 'f';
+        String string = "Hello World!";
+
+        // Print Out
+        System.out.println(isMikeIsFemale);
+        System.out.println(num1);
+        System.out.println(num2);
+        System.out.println(num3);
+        System.out.println(num4);
+        System.out.println(num5);
+        System.out.println(num6);
+        System.out.println(string);
+        System.out.println(char1);
     }
 }
