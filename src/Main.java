b77599805c0776af33dd860bd1aa4226c2883405
Frank Liu <lfl@lfs-MacBook-Air.local>
2022-09-29 15:36:36 +0800

    feat: scanner

--- src/Main.java
+++ src/Main.java
@@ -1,24 +1,19 @@
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        boolean isMikeIsFemale = true;
-        byte num1 = -100;
-        short num2 = 30000;
-        int num3 = 1000000000;
-        long num4 = 1000000000;
-        float num5 = 100.10000f;
-        double num6 = 10.30000111111111111111111;
-        char char1 = 'f';
-        String string = "Hello World!";
+        Scanner scanner = new Scanner(System.in);
 
-        // Print Out
-        System.out.println(isMikeIsFemale);
-        System.out.println(num1);
-        System.out.println(num2);
-        System.out.println(num3);
-        System.out.println(num4);
-        System.out.println(num5);
-        System.out.println(num6);
-        System.out.println(string);
-        System.out.println(char1);
+        System.out.println("What's your \n name?");
+        String name = scanner.nextLine();
+        System.out.println("Hello " + name);
+
+        System.out.println("How old are you?");
+        int age = scanner.nextInt();
+        System.out.println("Your age is " + age);
+
+        System.out.println("where are you living in?");
+        String country = scanner.nextLine();
+        System.out.println("Oh " + country + " must be a beautiful place.");
     }
 }
