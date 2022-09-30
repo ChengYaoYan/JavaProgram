56583ae6ef3806fa13e149e5ae52f08269bfcdb6
Frank Liu <lfl@lfs-MacBook-Air.local>
2022-09-30 10:40:18 +0800

    feat: hypotenuse with Scanner

--- src/Main.java
+++ src/Main.java
@@ -1,14 +1,30 @@
-import javax.swing.JOptionPane;
+// import javax.swing.JOptionPane;
+import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
-        String name = JOptionPane.showInputDialog("Enter your name");
-        JOptionPane.showMessageDialog(null, "Hello" + name);
+//        String name = JOptionPane.showInputDialog("Enter your name");
+//        JOptionPane.showMessageDialog(null, "Hello" + name);
+//
+//        int age = Integer.parseInt(JOptionPane.showInputDialog("Enter your age"));
+//        JOptionPane.showMessageDialog(null, "you are " + age + " years old");
+//
+//        double height = Double.parseDouble(JOptionPane.showInputDialog("Enter your height"));
+//        JOptionPane.showMessageDialog(null, "you are " + height + "cm height");
 
-        int age = Integer.parseInt(JOptionPane.showInputDialog("Enter your age"));
-        JOptionPane.showMessageDialog(null, "you are " + age + " years old");
+        double x;
+        double y;
+        double z;
 
-        double height = Double.parseDouble(JOptionPane.showInputDialog("Enter your height"));
-        JOptionPane.showMessageDialog(null, "you are " + height + "cm height");
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("Enter the side x: ");
+        x = scanner.nextDouble();
+
+        System.out.println("Enter the side y: ");
+        y = scanner.nextDouble();
+
+        z = Math.sqrt(x*x + y*y);
+        System.out.println("The side z is:  " + z);
     }
 }
