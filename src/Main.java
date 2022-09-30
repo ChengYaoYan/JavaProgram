b5c16dc959ab514ede1c601043c93b6522043257
Frank Liu <lfl@lfs-MacBook-Air.local>
2022-09-30 18:03:49 +0800

    feat: overloaded method

--- src/Main.java
+++ src/Main.java
@@ -1,12 +1,18 @@
-import java.util.ArrayList;
-
 public class Main {
     public static void main(String[] args) {
-        ArrayList<String> foods = new ArrayList<String>();
+        System.out.println(add(10, 20, 30, 40, 50));
+    }
 
-        foods.add("pizza");
-        foods.set(0, "banana");
-
-        foods.forEach(System.out::println);
+    static int add(int x, int y) {
+        return x + y;
+    }
+    static int add(int x, int y, int z) {
+        return x + y + z;
+    }
+    static int add(int x, int y, int z, int a) {
+        return x + y + z + a;
+    }
+    static int add(int x, int y, int z, int a, int b) {
+        return x + y + z + a + b;
     }
 }
