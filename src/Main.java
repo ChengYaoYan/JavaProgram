1f3b65bcbbb35db48b63da55c187043f1a3ff62c
Frank Liu <lfl@lfs-MacBook-Air.local>
2022-09-30 10:43:31 +0800

    feat: hypotenuse with JOptionPane

--- src/Main.java
+++ src/Main.java
@@ -1,30 +1,11 @@
-// import javax.swing.JOptionPane;
-import java.util.Scanner;
+import javax.swing.JOptionPane;
 
 public class Main {
     public static void main(String[] args) {
-//        String name = JOptionPane.showInputDialog("Enter your name");
-//        JOptionPane.showMessageDialog(null, "Hello" + name);
-//
-//        int age = Integer.parseInt(JOptionPane.showInputDialog("Enter your age"));
-//        JOptionPane.showMessageDialog(null, "you are " + age + " years old");
-//
-//        double height = Double.parseDouble(JOptionPane.showInputDialog("Enter your height"));
-//        JOptionPane.showMessageDialog(null, "you are " + height + "cm height");
+        double x  = Double.parseDouble(JOptionPane.showInputDialog("Enter the side x"));
+        double y  = Double.parseDouble(JOptionPane.showInputDialog("Enter the side y"));
+        double z = Math.sqrt(x*x + y*y);
 
-        double x;
-        double y;
-        double z;
-
-        Scanner scanner = new Scanner(System.in);
-
-        System.out.println("Enter the side x: ");
-        x = scanner.nextDouble();
-
-        System.out.println("Enter the side y: ");
-        y = scanner.nextDouble();
-
-        z = Math.sqrt(x*x + y*y);
-        System.out.println("The side z is:  " + z);
+        JOptionPane.showMessageDialog(null, "hypotenuse is " + z);
     }
 }
