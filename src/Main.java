45cd0804c26761d003ff0e269029ff636ea9adac
Frank Liu <lfl@lfs-MacBook-Air.local>
2022-09-30 11:16:25 +0800

    feat: if switch for while statements

--- src/Main.java
+++ src/Main.java
@@ -1,11 +1,45 @@
-import javax.swing.JOptionPane;
+import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
-        double x  = Double.parseDouble(JOptionPane.showInputDialog("Enter the side x"));
-        double y  = Double.parseDouble(JOptionPane.showInputDialog("Enter the side y"));
-        double z = Math.sqrt(x*x + y*y);
+//        Scanner scanner = new Scanner(System.in);
+//
+//        System.out.println("Enter your grade: ");
+//        String grade = scanner.nextLine();
 
-        JOptionPane.showMessageDialog(null, "hypotenuse is " + z);
+        // if
+//        if (grade <= 10) {
+//            System.out.println("Very Bad");
+//        } else if (grade <= 60) {
+//            System.out.println("Not Passed");
+//        } else if (grade <= 90) {
+//            System.out.println("Great");
+//        } else {
+//            System.out.println("Perfect");
+//        }
+
+        // while
+//        while (grade < 100) {
+//            System.out.println("I'm working hard");
+//            grade++;
+//        }
+
+
+        // switch
+//        switch (grade) {
+//            case "D" -> System.out.println("Very Bad");
+//            case "C" -> System.out.println("Not Passed");
+//            case "B" -> System.out.println("Great");
+//            case "A" -> System.out.println("Perfect");
+//            default -> {
+//            }
+//        }
+
+        // for
+        for (int i = 0; i < 100; i++) {
+            for (int j = 0; j < 100; j++) {
+                System.out.println(i*j);
+            }
+        }
     }
 }
