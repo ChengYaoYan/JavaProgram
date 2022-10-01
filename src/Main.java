90c7e953ae822441c09094276b422c669a6c2c45
Frank Liu <lfl@lfs-MacBook-Air.local>
2022-10-01 11:52:08 +0800

    feat: printf

--- src/Main.java
+++ src/Main.java
@@ -1,18 +1,5 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println(add(10, 20, 30, 40, 50));
-    }
-
-    static int add(int x, int y) {
-        return x + y;
-    }
-    static int add(int x, int y, int z) {
-        return x + y + z;
-    }
-    static int add(int x, int y, int z, int a) {
-        return x + y + z + a;
-    }
-    static int add(int x, int y, int z, int a, int b) {
-        return x + y + z + a + b;
+        System.out.printf("Hello %1s %2s", "Jack", "Mike");
     }
 }
